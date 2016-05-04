af45064081334900d82b0312da5a9d5af99e6460
jag028 <jag.gmz028@gmail.com>
2016-05-04 11:19:38 -0700

    added the onStartCommand

--- app/src/main/java/com/example/jag028/service_test/DemoService.java
+++ app/src/main/java/com/example/jag028/service_test/DemoService.java
@@ -3,9 +3,16 @@ package com.example.jag028.service_test;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class DemoService extends Service {
     public DemoService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        Toast.makeText(DemoService.this, "Service Started", Toast.LENGTH_SHORT).show();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
